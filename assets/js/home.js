44d06870c6ec49b5a4cc8d48bcbfa0d275b680b9
Agshinhummatov <agshinshh@code.edu.az>
2023-03-02 17:08:44 +0400

    update

--- assets/js/home.js
+++ assets/js/home.js
@@ -23,120 +23,60 @@ $(document).ready(function () {
   });
 
 
-  //slider
 
 
 
-  $("#slideshow > div:gt(0)").hide();
 
-  var buttons = "<button class=\"slidebtn prev\"><i class=\"fa fa-chevron-circle-left\"></i></button><button class=\"slidebtn next\"><i class=\"fa fa-chevron-circle-right\"></i></button\>";
 
-  var slidesl = $('.slideitem').length
-  var d = "<li class=\"dot active-dot\">&bull;</li>";
-  for (var i = 1; i < slidesl; i++) {
-    d = d + "<li class=\"dot\">&bull;</li>";
-  }
-  var dots = "<ul class=\"slider-dots\">" + d + "</ul\>";
+  //cardsilder
 
-  $("#slideshow").append(dots).append(buttons);
-  var interval = setInterval(slide, 3000);
 
-  function intslide(func) {
-    if (func == 'start') {
-      interval = setInterval(slide, 3000);
-    } else {
-      clearInterval(interval);
+  $('.carousel .carousel-item').each(function () {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
     }
-  }
+    next.children(':first-child').clone().appendTo($(this));
 
-  function slide() {
-    sact('next', 0, 1200);
-  }
-
-  function sact(a, ix, it) {
-    var currentSlide = $('.current');
-    var nextSlide = currentSlide.next('.slideitem');
-    var prevSlide = currentSlide.prev('.slideitem');
-    var reqSlide = $('.slideitem').eq(ix);
-
-    var currentDot = $('.active-dot');
-    var nextDot = currentDot.next();
-    var prevDot = currentDot.prev();
-    var reqDot = $('.dot').eq(ix);
-
-    if (nextSlide.length == 0) {
-      nextDot = $('.dot').first();
-      nextSlide = $('.slideitem').first();
-    }
-
-    if (prevSlide.length == 0) {
-      prevDot = $('.dot').last();
-      prevSlide = $('.slideitem').last();
-    }
-
-    if (a == 'next') {
-      var Slide = nextSlide;
-      var Dot = nextDot;
-    }
-    else if (a == 'prev') {
-      var Slide = prevSlide;
-      var Dot = prevDot;
-    }
-    else {
-      var Slide = reqSlide;
-      var Dot = reqDot;
-    }
-
-    currentSlide.fadeOut(it).removeClass('current');
-    Slide.fadeIn(it).addClass('current');
-
-    currentDot.removeClass('active-dot');
-    Dot.addClass('active-dot');
-  }
-
-  $('.next').on('click', function () {
-    intslide('stop');
-    sact('next', 0, 400);
-    intslide('start');
-  });//next
-
-  $('.prev').on('click', function () {
-    intslide('stop');
-    sact('prev', 0, 400);
-    intslide('start');
-  });//prev
-
-  $('.dot').on('click', function () {
-    intslide('stop');
-    var index = $(this).index();
-    sact('dot', index, 400);
-    intslide('start');
-  });//prev
-  //slideshow
-
-
-
-
-
-});
-
-
-
-
-const optionMenu = document.querySelector(".select-menu"),
-  selectBtn = optionMenu.querySelector(".select-btn"),
-  options = optionMenu.querySelectorAll(".option"),
-  sBtn_text = optionMenu.querySelector(".sBtn-text");
-
-selectBtn.addEventListener("click", () =>
-  optionMenu.classList.toggle("active")
-);
-
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    let selectedOption = option.querySelector(".option-text").innerText;
-    sBtn_text.innerText = selectedOption;
-
-    optionMenu.classList.remove("active");
+    for (var i = 0; i < minPerSlide; i++) { next = next.next(); if (!next.length) { next = $(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); }
   });
+
+
+
+
+  //silder 
+
+
+  var swiper = new Swiper(".mySwiper", {
+    loop:true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
