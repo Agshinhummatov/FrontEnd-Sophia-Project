f0eef9a41a07715389a516acf391ead336012303
Agshinhummatov <agshinshh@code.edu.az>
2023-03-09 02:48:15 +0400

    update

--- assets/js/product.js
+++ assets/js/product.js
@@ -18,6 +18,35 @@ $(document).ready(function () {
 
 
   //slide
+
+ 
+
+
+
+  //tab menu 
+
+  const tabLink = document.querySelectorAll(".tab-menu-link");
+const tabContent = document.querySelectorAll(".tab-bar-content");
+
+tabLink.forEach((item) => {
+  item.addEventListener("click", activeTab);
+});
+
+function activeTab(item) {
+  const btnTarget = item.currentTarget;
+  const content = btnTarget.dataset.content;
+
+  tabContent.forEach((item) => {
+    item.classList.remove("is-active");
+  });
+
+  tabLink.forEach((item) => {
+    item.classList.remove("is-active");
+  });
+
+  document.querySelector("#" + content).classList.add("is-active");
+  btnTarget.classList.add("is-active");
+}
   
  
 
